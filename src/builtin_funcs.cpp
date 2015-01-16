f72a79362c059df9f1fdb4f23022448c81adc7c0
Benjamin <brpylko@gmail.com>
2015-01-15 19:24:23 -0500

    began implementing a joystick component

--- src/builtin_funcs.cpp
+++ src/builtin_funcs.cpp
@@ -1,5 +1,6 @@
 #include "kzqcvm.h"
 #include "data.h"
+#include "devicevectors.hpp"
 #include <iostream>
 
 using namespace kzqcvm;
@@ -23,18 +24,39 @@ bool vm_backtrace(Kzqcvm* vm, int funcnum)
 {
 	StringPointer s = vm->GetParameterStringPointer(0);
 	cerr << "Warn: not actually backtracing, here's the error message: " << s.Get() << endl;
+	return true;
 }
 
 bool vm_error(Kzqcvm* vm, int funcnum)
 {
 	StringPointer s = vm->GetParameterStringPointer(0);
-	cerr << s.Get() << endl;
+	cerr << s.Get().GetValue() << endl;
+	return true;
 }
 
 bool vm_print(Kzqcvm* vm, int funcnum)
 {
 	StringPointer s = vm->GetParameterStringPointer(0);
-	cout << s.Get();
+	cout << s.Get().GetValue();
+	return true;
+}
+
+bool vm_strcat(Kzqcvm* vm, int funcnum)
+{
+	StringPointer s1 = vm->GetParameterStringPointer(0);
+	StringPointer s2 = vm->GetParameterStringPointer(1);
+	StringPointer ret = vm->GetReturnStringPointer();
+	ret.Set(vm->TempString(string(s1.Get().GetValue())+string(s2.Get().GetValue())));
+	return true;
+}
+
+bool vm_joystick_builtin(Kzqcvm* vm, int funcnum)
+{
+	if(joysticks[vm->GetParameterFloatPointer(0)] == NULL)
+	{
+		joysticks[vm->GetParameterFloatPointer(0)] = new Joystick(vm->GetParameterFloatPointer(0));
+	}
+	joysticks[vm->GetParameterFloatPointer(0)]->GetX();
 }
 
 void register_builtins(Kzqcvm* vm)
@@ -43,4 +65,6 @@ void register_builtins(Kzqcvm* vm)
 	vm->AddBuiltin(vm_remove, 2);
 	vm->AddBuiltin(vm_backtrace, 3);
 	vm->AddBuiltin(vm_error, 4);
+	vm->AddBuiltin(vm_print, 5);
+	vm->AddBuiltin(vm_strcat, 6);
 }
---
+++ src/devicevectors.hpp
@@ -0,0 +1,3 @@
+#include "WPILib.h"
+
+extern vector<Joystick*> joysticks;
